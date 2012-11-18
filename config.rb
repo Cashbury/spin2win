d77f43048207d9d58be8f2c491c0c0a0a555f4d9
ralevt <dpaskorz@gmail.com>
2012-11-18 10:50:22 -0500

    Symbol assignment to payline.

--- config.rb
+++ config.rb
@@ -18,13 +18,30 @@ class ConfigSlot
     @@l
   end
 
+  @@symbol = Hash.new
+  def self.symbol
+    @@symbol
+  end
+
   # 1) Define the maximum number of prizes for each prize category
   n.prize_total = 2
   m.prize_total = 2
   l.prize_total = 2
 
   # 2) Set the symbols needed to run the game
-  # symbol_count = prize total + 1 
+  symbol_count = n.prize_total + m.prize_total + l.prize_total + 1 
+  symbol = Hash.new
+  for i in 1..symbol_count
+    symbol[:"s#{i}"] = nil # symbol assignment, e.g. '7' 
+  end
+  symbol[:s1] = 'space'
+  # temp, this needs to be input driven
+  symbol[:s2] = '7'
+  symbol[:s3] = 'bar'
+  symbol[:s4] = 'bell'
+  symbol[:s5] = 'cherry'
+  symbol[:s6] = 'star'
+  symbol[:s7] = 'x'
   
   # 3) Define the paylines
   n.payline_prob_array = [0.03, 0.02] 
@@ -35,22 +52,22 @@ class ConfigSlot
   m.payline_prob = Hash.new
   l.payline_prob = Hash.new
 
-  n.symbols = Array.new
-  m.symbols = Array.new
-  l.symbols = Array.new
+  n.symbol = Hash.new
+  m.symbol = Hash.new
+  l.symbol = Hash.new
 
   # Setting symbols and defining paylines
   for i in 1..n.prize_total
     n.payline_prob[:"n#{i}"] = n.payline_prob_array[i-1]
-    n.symbols.push :"n#{i}" 
+    n.symbol[:"n#{i}"] = :"s#{i+1}"  # i + 1 to skip over space
   end
   for i in 1..m.prize_total
     m.payline_prob[:"m#{i}"] = m.payline_prob_array[i-1]
-    m.symbols.push :"m#{i}"
+    m.symbol[:"m#{i}"] = :"s#{i+1+n.prize_total}"
   end
   for i in 1..l.prize_total
     l.payline_prob[:"l#{i}"] = l.payline_prob_array[i-1] 
-    l.symbols.push :"l#{i}"
+    l.symbol[:"l#{i}"] = :"s#{i+1+n.prize_total+m.prize_total}"
   end
 
   # 4) Define the prizes, how do I make a general hash with these specific keys and they will be 
